07dfdf5af933f215750f7e12c5dd794a4afe00f7
Shib <contact@shib.cc>
2021-08-04 20:31:14 +0200

    Initial  commit

---
+++ configure.sh
@@ -0,0 +1,1 @@
+cmake -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl@1.1/ -Wno-dev -S . -B out/build
--- main.cc
+++
@@ -1,9 +0,0 @@
-#include "iostream"
-
-int main(int argc, char const *argv[]) {
-  for ( int i = 0; i <= argc; i++ ) {
-    std::cout << i << '\n';
-    std::cout << argv[i] << '\n';
-  }
-  return 0;
-}
